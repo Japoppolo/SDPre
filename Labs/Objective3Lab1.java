65c679f1f56e8bae88aa0f51007c00c06964ced3
David Nawrocki <the.ellimist.lives@gmail.com>
2020-08-14 15:08:47 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,10 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 77;
+    num2 = 49;
+    num3 = 18;
+    average = (num1 + num2 + num3)/3;
+    System.out.println(average);
+  }
+}

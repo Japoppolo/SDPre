93e6cfb71a45eb58bf65cd1240344569413c2721
David Nawrocki <the.ellimist.lives@gmail.com>
2020-08-14 13:44:42 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "David Nawrocki";
+    int age = 33;
+    String state = "Michigan";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}

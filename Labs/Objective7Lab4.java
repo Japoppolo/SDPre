8cfe53b8d4688bf7c1330504128de18dbce8f789
David Nawrocki <the.ellimist.lives@gmail.com>
2020-08-17 08:59:28 -0500

    finish objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,16 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int count = 0, sum = 0;
+
+
+
+    while (count <= 20) {
+      System.out.println(sum);
+      count = count + 1;
+      sum = sum + count;
+
+
+
+    }
+  }
+}

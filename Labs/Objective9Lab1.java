b0dc6ea5efaa36f25f6d681c3eedf28ec7ad9a51
David Nawrocki <the.ellimist.lives@gmail.com>
2020-08-17 19:15:21 -0500

    finish objective9lab1

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1 {
+  public static void main(String[] args) {
+    printHello();
+  }
+
+  public static void printHello() {
+   System.out.println("Hello Doug");  
+
+  }
+}
